13b815a70d3396d27e7077b2b44716c437d5d0b6
b-t-barnes <bigbbarnes@hotmail.com>
2017-09-03 17:15:48 +1200

    set the dummy screen as background, added 3 invisible buttons over 'Track Parcel' "prompts" that launch the Tracking Page activity (not working yet)  with the bottom button launching the settings page (for testing).

--- app/src/main/java/com/example/brooke/thewarehousetrackingapp/TrackingPage.java
+++ app/src/main/java/com/example/brooke/thewarehousetrackingapp/TrackingPage.java
@@ -1,7 +1,9 @@
 package com.example.brooke.thewarehousetrackingapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class TrackingPage extends AppCompatActivity {
 
@@ -9,5 +11,38 @@ public class TrackingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking_page);
+
+        Toast.makeText(TrackingPage.this, "Track your Parcel here", Toast.LENGTH_LONG).show();
+
+
+
+    }
+
+    @Override
+    protected void onStart() { super.onStart();}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
