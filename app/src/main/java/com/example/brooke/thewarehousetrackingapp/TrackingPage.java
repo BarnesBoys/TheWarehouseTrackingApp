d05ca7c6c7591c9642c8f0478d563389cfe8cad3
b-t-barnes <bigbbarnes@hotmail.com>
2017-09-06 14:01:13 +1200

    added the dummy screen image to drawable folder

--- app/src/main/java/com/example/brooke/thewarehousetrackingapp/TrackingPage.java
+++ app/src/main/java/com/example/brooke/thewarehousetrackingapp/TrackingPage.java
@@ -1,9 +1,22 @@
 package com.example.brooke.thewarehousetrackingapp;
 
-import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+
 
 public class TrackingPage extends AppCompatActivity {
 
@@ -11,38 +24,96 @@ public class TrackingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking_page);
-
-        Toast.makeText(TrackingPage.this, "Track your Parcel here", Toast.LENGTH_LONG).show();
-
-
-
     }
 
-    @Override
-    protected void onStart() { super.onStart();}
-
-    @Override
-    protected void onResume() {
-        super.onResume();
+    public void trackingItem(View view) {
+        new getTrackingDetails().execute();
     }
+    class getTrackingDetails extends AsyncTask<String, String, String>{
+        protected String doInBackground(String... params){
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
+            try {
+                URL url = new URL("https://oauth.nzpost.co.nz/as/token.oauth2");
+                HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
+                SSLContext sc = SSLContext.getInstance("TLS");
+                sc.init(null, null, new java.security.SecureRandom());
+                conn.setSSLSocketFactory(sc.getSocketFactory());
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-    }
+                //set request properties
+                conn.setReadTimeout(7000);
+                conn.setConnectTimeout(7000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+                //transfer the authoriztion details as needed
+                DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+                out.writeBytes("grant_type=client_credentials&client_id=ccc7bf438e5f440dba9b0194b75a9fca&client_secret=dD021693C7f0410AbdAb48E63fD884c3");
+                out.flush();
+                out.close();
+
+                /* this code is to see if the response is alright
+                int responseCode = conn.getResponseCode();
+                System.err.println("code is: --------> " + responseCode);*/
+
+                //collect returned data
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line = "";
+                String json = "";
+                while((line = in.readLine()) != null){
+                    System.err.println(line);
+                    json += line;
+                }
+                in.close();
+
+                //put returned data in jsonobj
+                JSONObject obj = new JSONObject(json);
+                //extract the necessary access_token for interaction with nzpost api
+                String accessToken = obj.getString("access_token");
+                System.err.println(accessToken);
+                String holdParcelURL = "https://api.nzpost.co.nz/parceltrack/3.0/parcels/MR640187089NZ";
+
+                url = new URL(holdParcelURL);
+                conn = (HttpsURLConnection) url.openConnection();
+                sc = SSLContext.getInstance("TLS");
+                sc.init(null, null, new java.security.SecureRandom());
+                conn.setSSLSocketFactory(sc.getSocketFactory());
+
+                conn.setReadTimeout(7000);
+                conn.setConnectTimeout(7000);
+                conn.setRequestMethod("GET");
+                //conn.setDoInput(true);
+                //conn.setDoOutput(true);
+                conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+                int responseCode = conn.getResponseCode();
+                String responseMessage = "";
+
+                //printing out the header to see what is being set
+                Map<String, List<String>> map = conn.getHeaderFields();
+
+                for(Map.Entry<String, List<String>> entry : map.entrySet()){
+                    System.err.println(entry.getKey() + " : " + entry.getValue());
+                }
+
+                //return the actually json data or the error json data
+                if(responseCode != 200) in = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                else in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                while((line = in.readLine()) !=null){
+                    responseMessage += line;
+                }
+                //String responseMes = conn.getResponseMessage(); no message returned
+                System.err.println("code is: --------> " + responseCode);
+                System.err.println("message is ------>" + responseMessage);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
+
+
